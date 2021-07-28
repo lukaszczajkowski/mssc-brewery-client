4735fc2d88f10b4a31ee4f0e5d1155f566ec6638
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2021-07-28 13:17:53 +0200

    BreweryClient.saveNewBeer() and BreweryClient.updateBeer() methods implemented and tested

--- src/main/java/guru/springframework/msscbreweryclient/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/client/BreweryClient.java
@@ -6,10 +6,11 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Component
-@ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
+@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
 public class BreweryClient {
 
     public final String BEER_PATH_V1 = "/api/v1/beer/";
@@ -23,6 +24,14 @@ public class BreweryClient {
 
     public BeerDto getBeerById(UUID id) {
         return restTemplate.getForObject(apihost + BEER_PATH_V1 + id.toString(), BeerDto.class);
+    }
+
+    public URI saveNewBeer(BeerDto beerDto) {
+        return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
+    }
+
+    public void updateBeer(UUID id, BeerDto beerDto) {
+        restTemplate.put(apihost + BEER_PATH_V1 + id.toString(), beerDto);
     }
 
     public void setApihost(String apihost) {
