8e0beb81ec8e92ea4d6ac44a6c8b005bfc9429a1
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2021-07-28 11:52:10 +0200

    BreweryClient class added

---
+++ src/main/java/guru/springframework/msscbreweryclient/client/BreweryClient.java
@@ -0,0 +1,31 @@
+package guru.springframework.msscbreweryclient.client;
+
+import guru.springframework.msscbreweryclient.model.BeerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+@ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
+public class BreweryClient {
+
+    public final String BEER_PATH_V1 = "/api/v1/beer/";
+    private String apihost;
+
+    private final RestTemplate restTemplate;
+
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeerById(UUID id) {
+        return restTemplate.getForObject(apihost + BEER_PATH_V1 + id.toString(), BeerDto.class);
+    }
+
+    public void setApihost(String apihost) {
+        this.apihost = apihost;
+    }
+}
